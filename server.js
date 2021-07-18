0fd7c3eec6b5ddfc28834d9d6939d7feae3463ad
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-07-18 15:16:58 -0600

    delete route not working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware
